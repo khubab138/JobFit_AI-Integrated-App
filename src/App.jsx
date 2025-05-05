349571b94162aab3123bafc99c738acadb893ea7
Khubab138 <muhammadkhubabsiddiqui@gmail.com>
2025-05-05 09:25:08 -0700

    FIX rteact Router for deploument

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <div className="overflow-x-hidden h-screen text-neutral-300 antialiased selection:bg-green-300 selection:text-green-800">
       <div class="absolute top-0 z-[-2] h-full w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container flex flex-col">
-        <BrowserRouter>
+        <BrowserRouter  basename="/JobFit_AI-Integrated-App">
         <Routes>
           <Route path="/" element={<Page1 /> } ></Route>
           <Route path="/f1" element={<Resumeform1 />} ></Route>
