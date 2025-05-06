7ab31827f0471f59d5291af61b5383cb3621e277
Khubab138 <muhammadkhubabsiddiqui@gmail.com>
2025-05-06 14:31:17 -0700

    b

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import { BrowserRouter , Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-        <BrowserRouter  basename="/JobFit_AI-Integrated-App">
     <div className="overflow-x-hidden h-screen text-neutral-300 antialiased selection:bg-green-300 selection:text-green-800">
       <div class="absolute top-0 z-[-2] h-full w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container flex flex-col">
+      <BrowserRouter  basename="/JobFit_AI-Integrated-App">
         <Routes>
           <Route path="/" element={<Page1 /> } ></Route>
           <Route path="/f1" element={<Page2 />} ></Route>
         </Routes>
+        </BrowserRouter>
       </div>
     </div>
-        </BrowserRouter>
   );
 };
 
