6f3464151a79c9605d7db87a7d38368930007b6d
Khubab138 <muhammadkhubabsiddiqui@gmail.com>
2025-05-07 07:59:17 -0700

    set case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <div className="overflow-x-hidden h-screen text-neutral-300 antialiased selection:bg-green-300 selection:text-green-800">
       <div className="absolute top-0 z-[-2] h-full w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container flex flex-col">
-        <BrowserRouter  basename="/JobFit_AI-Integrated-App">
+        <BrowserRouter  basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Page1 /> } ></Route>
           <Route path="/f1" element={<Page2 />} ></Route>
--- src/Components/Resume_Forms/Resumeform1.jsx
+++ src/Components/Resume_Forms/Resumeform1.jsx
@@ -14,7 +14,7 @@ import {
   resetForm,
 } from "../../Redux/ResumeSlices";
 import { BiLogOut } from "react-icons/bi";
-import logo from "../../assets/Logo/l.svg"
+import logo from "../../assets/Logo/L.svg"
 
 const ResumeForm1 = () => {
   const isFormValid = () => {
